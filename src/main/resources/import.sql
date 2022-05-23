cef0cf63af8061afce8690ff53d51bc782632e61
Seth Karim Luis Martínez <seth.luis@banregio.com>
2022-05-22 23:33:04 -0500

    Se agrega archivo de inserción de datos por defecto

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+/* Insertar divisas por defecto*/
+insert into divisas (nombre, abreviacion, fecha_guardado) values('Peso Mexicano', 'MXN', now());
+insert into divisas (nombre, abreviacion, fecha_guardado) values('Dólar Estadounidense', 'USD', now());
+insert into divisas (nombre, abreviacion, fecha_guardado) values('Euro', 'EUR', now());
